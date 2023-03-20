d0b98d518170a54d623da1b84637aea3c29786de
Shashwat Johari <jshashwat93@gmail.com>
2023-03-20 18:15:49 -0400

    Initial commit

---
+++ src/coinbase-btc.py
@@ -0,0 +1,67 @@
+from confluent_kafka import Producer
+from confluent_kafka.serialization import SerializationContext, MessageField
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+import os
+import requests
+import time
+
+# Run the following in your terminal before running this script:
+# export BOOTSTRAP_SERVERS=<Bootstrap Server>
+# export SASL_USERNAME=<API Key>
+# export SASL_PASSWORD=<API Secret>
+# export SCHEMA_REGISTRY_URL=<Schema Registry URL>
+# export BASIC_AUTH_USER_INFO=<Schema Reistry API Key>:<Schema Registry API Secret>
+
+bootstrap_servers = os.environ['BOOTSTRAP_SERVERS']
+sasl_username = os.environ['SASL_USERNAME']
+sasl_password = os.environ['SASL_PASSWORD']
+schema_registry_url = os.environ['SCHEMA_REGISTRY_URL']
+basic_auth_user_info = os.environ['BASIC_AUTH_USER_INFO']
+
+producer = Producer({"bootstrap.servers": bootstrap_servers,
+                     'security.protocol': 'SASL_SSL', 
+                     'sasl.mechanisms': 'PLAIN', 
+                     'sasl.username': sasl_username, 
+                     'sasl.password': sasl_password})
+
+schema_registry_client = SchemaRegistryClient({'url': schema_registry_url, 
+                                               'basic.auth.user.info': basic_auth_user_info})
+
+schema_str = """
+    {
+        "type": "record",
+        "name": "BitcoinPriceUpdate",
+        "namespace": "com.example",
+        "fields": [
+            { "name": "name", "type": "string" },
+            { "name": "price", "type": "double" }
+        ]
+    }
+"""
+
+count_avro_serializer = AvroSerializer(schema_registry_client = schema_registry_client,
+                                        schema_str =  schema_str,
+                                        to_dict = None)
+
+while True:
+    url = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        crypto_data = {"name": "Bitcoin", "price": float(data["data"]["amount"])}
+
+        producer.produce("coinbase-btc", 
+                    key="btc", 
+                    value=count_avro_serializer(crypto_data, SerializationContext("coinbase-btc", MessageField.VALUE)))
+        producer.poll(10000)
+        producer.flush()
+        time.sleep(5)
+    else:
+        print("Error: Unable to fetch data from Coinbase API.")
+
+
+    
+
+
---
+++ src/coingecko-btc.py
@@ -0,0 +1,84 @@
+from confluent_kafka import Producer
+from confluent_kafka.serialization import SerializationContext, MessageField
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+import os
+import requests
+import time
+
+# Run the following in your terminal before running this script:
+# export BOOTSTRAP_SERVERS=<Bootstrap Server>
+# export SASL_USERNAME=<API Key>
+# export SASL_PASSWORD=<API Secret>
+# export SCHEMA_REGISTRY_URL=<Schema Registry URL>
+# export BASIC_AUTH_USER_INFO=<Schema Reistry API Key>:<Schema Registry API Secret>
+
+bootstrap_servers = os.environ['BOOTSTRAP_SERVERS']
+sasl_username = os.environ['SASL_USERNAME']
+sasl_password = os.environ['SASL_PASSWORD']
+schema_registry_url = os.environ['SCHEMA_REGISTRY_URL']
+basic_auth_user_info = os.environ['BASIC_AUTH_USER_INFO']
+
+producer = Producer({"bootstrap.servers": bootstrap_servers,
+                     'security.protocol': 'SASL_SSL', 
+                     'sasl.mechanisms': 'PLAIN', 
+                     'sasl.username': sasl_username, 
+                     'sasl.password': sasl_password})
+
+schema_registry_client = SchemaRegistryClient({'url': schema_registry_url, 
+                                               'basic.auth.user.info': basic_auth_user_info})
+
+schema_str = """
+    {
+        "type": "record",
+        "name": "CryptoData",
+        "fields": [
+            {"name": "name", "type": "string"},
+            {"name": "price", "type": "double"},
+            {"name": "one_hour_price_change", "type": "double"}
+        ]
+    }
+"""
+
+count_avro_serializer = AvroSerializer(schema_registry_client = schema_registry_client,
+                                        schema_str =  schema_str,
+                                        to_dict = None)
+
+
+import requests
+import time
+import logging
+
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
+url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&price_change_percentage=1h"
+
+while True:
+    try:
+        response = requests.get(url)
+        data = response.json()
+
+        for item in data:
+            try:
+                crypto_data = {
+                    "name": item["name"],
+                    "price": item["current_price"],
+                    "one_hour_price_change": item["price_change_percentage_1h_in_currency"]
+                }
+            except Exception as e:
+                logging.error(f"Error processing data: {str(e)}. Data: {item}")
+
+
+        producer.produce("coingecko-btc", 
+                         key="btc", 
+                         value=count_avro_serializer(crypto_data, SerializationContext("coingecko-btc", MessageField.VALUE)))
+        producer.poll(10000)
+        producer.flush()
+
+        time.sleep(5)  # Wait for 1 second(s) before fetching data again
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error fetching data from API: {str(e)}")
+        
+    except Exception as e:
+        print(f"Error fetching data: {str(e)}")
