8d63bdfdde84dc6d09a9514393a34017fd72d73a
Shashwat Johari <jshashwat93@gmail.com>
2023-03-20 21:46:22 -0400

    Gamify display message

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,21 @@ echo
 BASIC_AUTH_USER_INFO="${SCHEMA_REGISTRY_API_KEY}:${SCHEMA_REGISTRY_API_SECRET}"
 export BASIC_AUTH_USER_INFO
 
-echo "Starting Coinbase and Coingecko Producers. Keep an eye on the 'coinbase-btc' and 'coingecko-btc' topics in your Confluent Cloud cluster to see real-time financial data analytics in action! Get ready to explore use cases such as crypto arbitrage and volatility analysis in a hands-on environment!"
+echo -e "\e[1;33m######################################################################\e[0m"
+echo -e "\e[1;33m#\e[0m                                                                   \e[1;33m#\e[0m"
+echo -e "\e[1;33m#\e[0m  \e[1;32m$$$$$$$\\  $$$$$$\\  $$\\   $$\\ $$$$$$$$\\ $$$$$$$$\\ $$$$$$\\  \e[1;33m#\e[0m"
+echo -e "\e[1;33m#\e[0m  \e[1;32m$$  __$$\\$$  __$$\\ $$ | $$  |$$  _____|$$  _____|\\_$$  _| \e[1;33m#\e[0m"
+echo -e "\e[1;33m#\e[0m  \e[1;32m$$ |  $$ |$$ /  $$ |$$ |$$  / $$ |      $$ |        $$ |   \e[1;33m#\e[0m"
+echo -e "\e[1;33m#\e[0m  \e[1;32m$$ |  $$ |$$ |  $$ |$$$$$  /  $$$$$\\    $$$$$\\      $$ |   \e[1;33m#\e[0m"
+echo -e "\e[1;33m#\e[0m  \e[1;32m$$ |  $$ |$$ |  $$ |$$  _$$<   $$  __|   $$  __|     $$ |   \e[1;33m#\e[0m"
+echo -e "\e[1;33m#\e[0m  \e[1;32m$$$$$$$  |$$$$$$$  |$$ | \\$$\\  $$$$$$$$\\ $$$$$$$$\\   $$ |   \e[1;33m#\e[0m"
+echo -e "\e[1;33m#\e[0m  \e[1;32m\\_______/ \\_______/ \\__|  \\__| \\________|\\________|  \\__|   \e[1;33m#\e[0m"
+echo -e "\e[1;33m#\e[0m                                                                   \e[1;33m#\e[0m"
+echo -e "\e[1;33m#\e[0m  \e[1;32mStarting Coinbase and Coingecko Producers...\e[0m                                     \e[1;33m#\e[0m"
+echo -e "\e[1;33m#\e[0m  \e[1;36mKeep an eye on the 'coinbase-btc' and 'coingecko-btc' topics in your Confluent Cloud cluster to see real-time financial data analytics in action!\e[0m   \e[1;33m#\e[0m"
+echo -e "\e[1;33m#\e[0m  \e[1;34mGet ready to explore use cases such as crypto arbitrage and volatility analysis in a hands-on environment!\e[0m                       \e[1;33m#\e[0m"
+echo -e "\e[1;33m#\e[0m                                                                   \e[1;33m#\e[0m"
+echo -
 python coinbase-btc.py & python coingecko-btc.py
 
 wait
