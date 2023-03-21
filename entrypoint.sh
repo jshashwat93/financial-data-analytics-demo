3fcaa5a86b5a5fee77aa37d2ac24789584e3bccd
Shashwat Johari <jshashwat93@gmail.com>
2023-03-20 21:35:35 -0400

    Adding prompts to indicate the producer is running

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,9 @@ echo
 BASIC_AUTH_USER_INFO="${SCHEMA_REGISTRY_API_KEY}:${SCHEMA_REGISTRY_API_SECRET}"
 export BASIC_AUTH_USER_INFO
 
+echo "Starting Coinbase and Coingecko Producers..."
 python coinbase-btc.py & python coingecko-btc.py
+echo "Our producers are up and running! Keep an eye on the 'coinbase-btc' and 'coingecko-btc' topics in your Confluent Cloud cluster to see real-time financial data analytics in action! Get ready to explore use cases such as crypto arbitrage and volatility analysis in a hands-on environment!"
 
 wait
 
