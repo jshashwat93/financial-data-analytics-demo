f6dc1a372683cf60ee064f9af229524213390cda
Shashwat Johari <jshashwat93@gmail.com>
2023-04-04 23:04:55 -0400

    Add finishing touches to warning message

--- terraform/destroy-confluent-cloud.sh
+++ terraform/destroy-confluent-cloud.sh
@@ -34,5 +34,5 @@ rm -f secrets.txt
 # Notify user to delete .env file when done
 
 echo -e "\n\n\033[1;31m\033[7m⚠️ IMPORTANT WARNING ⚠️\033[0m\033[1;31m"
-echo -e "\033[1;34mIf you are done with using Terraform for this demo, you should delete the \033[1;31m.env\033[1;34m file for security reasons. You can run \033[1;32mrm -f terraform/.env\033[1;34m to delete this file.\033[0m"
+echo -e "\033[1;34mIf you are done with using Terraform for this demo, you should delete the \033[1;31m.env\033[1;34m file for security reasons. You can run \033[1;32mrm -f terraform/.env\033[1;34m to delete this file.\033[0m\n\n\n"
 
