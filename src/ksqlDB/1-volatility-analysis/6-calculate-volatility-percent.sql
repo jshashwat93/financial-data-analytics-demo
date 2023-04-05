405fe8ef8aff544838d5cfd9d9489e4cdc26e7c2
Shashwat Johari <jshashwat93@gmail.com>
2023-04-04 22:17:06 -0400

    Fix bugs in Volatility app

--- src/ksqlDB/1-volatility-analysis/6-calculate-volatility-percent.sql
+++ src/ksqlDB/1-volatility-analysis/6-calculate-volatility-percent.sql
@@ -2,6 +2,6 @@
   WITH (KAFKA_TOPIC='imply_bitcoin_volatility')
 AS SELECT 
 NAME, event_timestamp, ABS(PRICE-FIVE_MINUTE_AVERAGE_PRICE) price_fluctuation, round(ABS(PRICE-FIVE_MINUTE_AVERAGE_PRICE)/FIVE_MINUTE_AVERAGE_PRICE*100,4) VOLATILITY_PERCENT, START_PERIOD_TS, END_PERIOD_TS
-  FROM tmp4
+  FROM current_price_join_average_price
   WHERE event_timestamp > START_PERIOD_TS and event_timestamp < END_PERIOD_TS
   EMIT CHANGES;
--- src/ksqlDB/1-volatility-analysis/7-identify-high-volatility-event.sql
+++ src/ksqlDB/1-volatility-analysis/7-identify-high-volatility-event.sql
@@ -1,6 +1,6 @@
   CREATE STREAM high_volatility_event
 AS SELECT 
 NAME, event_timestamp, VOLATILITY_PERCENT, START_PERIOD_TS, END_PERIOD_TS
-  FROM tmp8
+  FROM btc_volatility_percent
   WHERE VOLATILITY_PERCENT > 0.1
   EMIT CHANGES;
